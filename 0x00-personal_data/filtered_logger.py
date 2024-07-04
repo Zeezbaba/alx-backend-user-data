51215a226dc126c6248b9e230bacb1290bdee6a0
Zeezbaba <Azeez_okeleji>
2024-07-04 12:21:00 +0000

    personal-data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,12 +5,37 @@ returns the log message obfuscated
 
 import re
 from typing import List
+import logging
+
+# def filter_datum(fields, redaction, message, separator):
+#     """Returns the log message obfuscated"""
+#     pattern = f"({'|'.join(fields)})=[^;{separator}]*"
+#     return re.sub(pattern, lambda m: m.group().split('=')
+#                   [0] + '=' + redaction, message)
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """return the log message obfuscated"""
+    """Returns the log message obfuscated"""
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        messages = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            messages, self.SEPARATOR)
