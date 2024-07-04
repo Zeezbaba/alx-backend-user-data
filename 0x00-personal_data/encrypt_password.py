e735c9ec2d53dec8e4088f600dc0acb1c5fb4555
Zeezbaba <Azeez_okeleji>
2024-07-04 16:21:03 +0000

    personal-data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if provided password
+    matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
