47532fd1907e6c2e43516d504aea8bf0a3485df0
Zeezbaba <Azeez_okeleji>
2024-07-09 14:10:34 +0000

    Basic-Auth: task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,18 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """determine if a path require auth.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                if path == excluded_path:
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """retrieves auth. header from request
