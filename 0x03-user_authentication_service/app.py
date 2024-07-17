ade7d0787fb3c762b358d6c43615954e26bef4a5
Zeezbaba <Azeez_okeleji>
2024-07-17 23:25:21 +0000

    User-Auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,7 +96,7 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email,
                         "message": "Password updated"}), 200
-    except ValueError:
+    except Exception:
         abort(403)
 
 
